46ef06774533289e75cfee4240574998f02ec24f
Eterill <eterill@hotmail.com>
2025-01-29 19:14:24 +1000

    Add files via upload

--- Plugin.php
+++ Plugin.php
@@ -2,11 +2,12 @@
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
- * Git Widget For Typecho.
+ * Git Widget魔改版<span style='border-radius: 2px;padding: .10rem .20rem;margin: 0 .40rem 0 .40rem;position: relative;background-color: #E040FB;color: #fff;'><a href="http://eterill.us.kg" style="color:#fff;text-decoration:none;">by EternalIllusion</a></span>｜使用方法详见插件设置
+ * 
  *
  * @package GitWidget
  * @author Simon.H
- * @version 1.0.0
+ * @version Mod1.2
  * @link http://ywy.me
  */
 class GitWidget_Plugin implements Typecho_Plugin_Interface
@@ -20,8 +21,9 @@ class GitWidget_Plugin implements Typecho_Plugin_Interface
      */
     public static function activate()
     {
-        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('GitWidget_Plugin','parselable');
-        Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('GitWidget_Plugin','parselable');
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('GitWidget_Plugin','parselabelg');
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('GitWidget_Plugin','parselabelg');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array('GitWidget_Plugin','insertjs');
         Typecho_Plugin::factory('Widget_Archive')->footer = array('GitWidget_Plugin','footerjs');
     }
     
@@ -97,7 +99,7 @@ class GitWidget_Plugin implements Typecho_Plugin_Interface
     public static function footerjs(){
         echo '<script language="javascript">if (typeof(needGithubWidget) != "undefined") {
             var script = document.createElement("script");
-            script.src = "//cdn.bootcss.com/github-repo-widget/e23d85ab8f/jquery.githubRepoWidget.min.js";
+            script.src = "' . Helper::options()->pluginUrl . '/GitWidget/github.js"
             document.body.appendChild(script);
         }</script>';
     }
@@ -127,10 +129,29 @@ class GitWidget_Plugin implements Typecho_Plugin_Interface
 .osc_git_box{background-color: #fff;}
 .osc_git_box{border-color: #E3E9ED;}
 .osc_git_info{color: #666;}
-.osc_git_main a{color: #9B9B9B;}', _t('Gitee样式表'));
+.osc_git_main a{color: #9B9B9B;}
+.osc_git_footer{display:none;}', _t('Gitee样式表'));
         $form->addInput($giteecss);
         $githubcss = new Typecho_Widget_Helper_Form_Element_Textarea('github_css', NULL, '', _t('Github样式表'));
         $form->addInput($githubcss);
+        $importjq = new Typecho_Widget_Helper_Form_Element_Radio(
+            'importjq', 
+            array('1' => '引入', '0' => '不引入'),
+            '0', 
+            _t('自动引入jQuery'), 
+            _t('选择是否在自动引入jQuery（GitHub卡片需要，不建议在这引用，容易不工作）。')
+        );
+        $form->addInput($importjq);
+?>
+<p>
+<h3>使用方法：</h3><br />
+直接在文章中，添加gitwidget短代码:<br />
+[gitwidget type='gitee' url='SimonH/typecho-gitwidget']<br />
+[gitwidget type='github' url='JoelSutherland/GitHub-jQuery-Repo-Widget']<br />
+type:设置使用gitee还是github ｜ url:项目仓库地址<br />
+<br />
+</p>
+<?php
     }
 
     /**
@@ -141,4 +162,24 @@ class GitWidget_Plugin implements Typecho_Plugin_Interface
      * @return void
      */
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+	 * 输出js脚本
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public static function insertjs($widget)
+	{
+		$options = Helper::options();
+		$settings = $options->plugin('GitWidget');
+		//jquery模式
+		if ($settings->importjq) {
+		    if ($widget->is('single')) {
+			    echo ($settings->jqmode ? '<script type="text/javascript">
+window.jQuery || document.write(\'<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"><\/script>\')</script>' : '');
+		    }
+		}
+	}
 }
+?>
